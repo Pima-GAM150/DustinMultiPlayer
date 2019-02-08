92ae95985ec5fbbba9451f3a30c129ab05141a80
Unknown <dreed12@mail.pima.edu>
2019-02-08 14:44:56 -0700

    scenes connected
    Gets stuck after (OnConnectedToMaster)

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [BoxGroup("Game Control",true,true)]
+    public GameObject PauseMenu;
+
+    [BoxGroup("Game Control", true, true)]
+    public GameObject OptionsMenu;
+
+    public void StartConnection()
     {
-        
+        SceneManager.LoadScene("ServerConnection");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                Application.Quit();
+        #endif
+    }
+
+    public void MainMenu()
+    {
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Pause()
+    {
+        PauseMenu.SetActive(true);
+        Debug.Log("Pause");
+        Time.timeScale = .01f;
+    }
+
+    public void Resume()
+    {
+        PauseMenu.SetActive(false);
+        Debug.Log("Resume");
+        Time.timeScale = 1;
     }
 }
