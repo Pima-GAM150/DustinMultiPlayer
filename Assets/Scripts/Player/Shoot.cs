3dd1e648a6cf1723572bf0ae82b82ce6214344a5
Unknown <dreed12@mail.pima.edu>
2019-02-08 12:28:27 -0700

    made shoot script and made a terrain but deleted it

--- Assets/Scripts/Player/Shoot.cs
+++ Assets/Scripts/Player/Shoot.cs
@@ -1,8 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+using Photon.Pun;
 
-public class Shoot : MonoBehaviour
+public class Shoot : MonoBehaviourPun
 {
-    
+    [BoxGroup("Projectile",true,true)]
+    public GameObject Shell;
+
+    [BoxGroup("Projectile", true, true), LabelText("Spawn Location")]
+    public Transform SpawnTarget;
+
+    [BoxGroup("Projectile", true, true), LabelText("Player Color"),ReadOnly]
+    public Material PlayerColor;
+
+    private void Start()
+    {
+        PlayerColor = GetComponentInChildren<Renderer>().material;
+    }
+
+    private void Update()
+    {
+        if(!photonView.IsMine)
+        {
+            return;
+        }
+
+        if(Input.GetButtonDown("Fire1"))
+        {
+            photonView.RPC("FireShell", RpcTarget.All);
+        }
+    }
+
+    [PunRPC]
+    public void FireShell()
+    {
+        GameObject newShell = Instantiate(Shell, SpawnTarget.position, Quaternion.identity);
+
+        newShell.GetComponentInChildren<Renderer>().material = PlayerColor;
+    }
+
 }
